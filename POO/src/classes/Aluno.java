963b1c73dd651d33920cb77f1091d5c0f614d9c1
MatheusdeOliveiraCorrea <93480352+MatheusdeOliveiraCorrea@users.noreply.github.com>
2022-04-15 09:28:31 -0300

    adcionando mais atributos na classe Aluno

--- POO/src/classes/Aluno.java
+++ POO/src/classes/Aluno.java
@@ -1,15 +1,93 @@
 package classes;
 
+import java.util.Objects;
+
 public class Aluno {
 	
 	public double media; 
-	public double nota1;
-	public double nota2;
-	public double nota3;
-	public double nota4;
-	public int anoInicial;
-	public int anoFinal;
+	private String disciplina1;
+	private double nota1;
+	private String disciplina2;
+	private double nota2;
+	private String disciplina3;
+	private double nota3;
+	private String disciplina4;
+	private double nota4;
+	private int anoInicial;
+	private int anoFinal;
+	private String nome;
 	
+	
+	
+	public String getDisciplina1() {
+		return disciplina1;
+	}
+
+
+	public void setDisciplina1(String disciplina1) {
+		this.disciplina1 = disciplina1;
+	}
+
+
+	public String getDisciplina2() {
+		return disciplina2;
+	}
+
+
+	public void setDisciplina2(String disciplina2) {
+		this.disciplina2 = disciplina2;
+	}
+
+
+	public String getDisciplina3() {
+		return disciplina3;
+	}
+
+
+	public void setDisciplina3(String disciplina3) {
+		this.disciplina3 = disciplina3;
+	}
+
+
+	public String getDisciplina4() {
+		return disciplina4;
+	}
+
+
+	public void setDisciplina4(String disciplina4) {
+		this.disciplina4 = disciplina4;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(nome, other.nome);
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
 	public int getAnoInicial() {
 		return anoInicial;
 	}
@@ -73,7 +151,7 @@ public class Aluno {
 	
 	public double getMedia() {		
 		
-	   this.media = (nota1+nota2+nota3+nota4);
+	   this.media = (nota1+nota2+nota3+nota4)/4;
 		
 		return media;   
 	}
@@ -99,10 +177,10 @@ public class Aluno {
 
 	@Override
 	public String toString() {
-		return "Aluno [getAnoInicial()=" + getAnoInicial() + ", getAnoFinal()=" + getAnoFinal() + ", getNota1()="
-				+ getNota1() + ", getNota2()=" + getNota2() + ", getNota3()=" + getNota3() + ", getNota4()="
-				+ getNota4() + ", getMedia()=" + getMedia() + ", Aprovado()=" + Aprovado() + ", duracaoCurso()="
-				+ duracaoCurso() + "]";
+		return "Aluno [media=" + media + ", disciplina1=" + disciplina1 + ", nota1=" + nota1 + ", disciplina2="
+				+ disciplina2 + ", nota2=" + nota2 + ", disciplina3=" + disciplina3 + ", nota3=" + nota3
+				+ ", disciplina4=" + disciplina4 + ", nota4=" + nota4 + ", anoInicial=" + anoInicial + ", anoFinal="
+				+ anoFinal + ", nome=" + nome + "]";
 	}
 
 
--- POO/src/classes/Principal.java
+++ POO/src/classes/Principal.java
@@ -9,30 +9,42 @@ public class Principal {
 		
 		
 	   Aluno aluno = new Aluno();
+	   Aluno aluno2 = new Aluno();
+	   
 	   //pergunta
+	   String disciplina1 = JOptionPane.showInputDialog("disciplina : ");
 	   String nota1 = JOptionPane.showInputDialog("nota 1: ");
+	   String disciplina2 = JOptionPane.showInputDialog("disciplina : ");
 	   String nota2 = JOptionPane.showInputDialog("nota 2: ");
+	   String disciplina3 = JOptionPane.showInputDialog("disciplina : ");
 	   String nota3 = JOptionPane.showInputDialog("nota 3: ");
+	   String disciplina4 = JOptionPane.showInputDialog("disciplina : ");
 	   String nota4 = JOptionPane.showInputDialog("nota 4: ");
+	   
 	   
 	   //responde
 	   aluno.setNota1(Integer.parseInt(nota1));
 	   aluno.setNota2(Integer.parseInt(nota2));
 	   aluno.setNota3(Integer.parseInt(nota3));
 	   aluno.setNota4(Integer.parseInt(nota4));
+	   aluno.setDisciplina1(disciplina1);
+	   aluno.setDisciplina2(disciplina2);
+	   aluno.setDisciplina3(disciplina3);
+	   aluno.setDisciplina4(disciplina4);
 	   aluno.setAnoInicial(2022);
-	   aluno.setAnoFinal(2024);
+	   aluno.setAnoFinal(2024); 
 	   
-	  /* 
-	   * //mostra na tela
+	 
+	   //mostra na tela
 	   System.out.println("Media : " + aluno.getMedia());
 	   System.out.println("Aprovado? " + aluno.Aprovado());
 	   System.out.println("Duracao curso : " + aluno.duracaoCurso());
-	   */
+	   
 
 
 	   //mostra na tela
 	   System.out.println(aluno);
+	  
 	   
 	   
 	}
